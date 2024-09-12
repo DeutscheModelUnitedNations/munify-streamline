feb0dde47ecbd71bfac1140f4c749c8a7f9890a6
Tade Strehk <42637916+TadeSF@users.noreply.github.com>
2024-09-12 15:30:07 +0200

    Refactor helloWorldTask to use schedules and add cron expression

--- trigger/example.ts
+++ trigger/example.ts
@@ -1,14 +1,20 @@
-import { logger, task, wait } from "@trigger.dev/sdk/v3";
+import { logger, task, wait, schedules } from "@trigger.dev/sdk/v3";
 
-export const helloWorldTask = task({
+export const helloWorldTask = schedules.task({
   id: "hello-world",
+  cron: "*/5 * * * *", // Every 5 minutes
+
   run: async (payload: any, { ctx }) => {
     logger.log("Hello, world!", { payload, ctx });
 
     await wait.for({ seconds: 5 });
 
+    if (Math.random() > 0.6) {
+      throw new Error("Random error");
+    }
+
     return {
       message: "Hello, world!",
-    }
+    };
   },
 });
